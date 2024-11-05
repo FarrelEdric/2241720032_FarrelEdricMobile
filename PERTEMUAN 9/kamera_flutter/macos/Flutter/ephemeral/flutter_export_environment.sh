6bd12a48961cb8daf7d56cae0d9b8340c2bf1c25
FarrelEdric <franciscusfarrel14@gmail.com>
2024-11-05 07:58:47 +0700

    jobsit

---
+++ PERTEMUAN 9/kamera_flutter/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Farrel Edric\OneDrive\Documents\Semester 5\Pemograman Mobile\2241720032_FarrelEdricMobile\PERTEMUAN 9\kamera_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
